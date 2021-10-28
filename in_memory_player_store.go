8835869100b8898116de0620c09a2a3ead57e101
dthtien <tien.dau@employmenthero.com>
2021-10-28 21:39:43 +0700

    ft(server): add muext to sychronize read/write access

--- in_memory_player_store.go
+++ in_memory_player_store.go
@@ -1,17 +1,28 @@
 package main
 
+import "sync"
+
 type InMemoryPlayerStore struct {
   store map[string]int
+  lock sync.Mutex
 }
 
 func NewInMemoryPlayerStore() * InMemoryPlayerStore {
-  return &InMemoryPlayerStore{store: map[string]int{}}
+  return &InMemoryPlayerStore{
+    store: map[string]int{},
+    // A mutex is used to synchronize read/write access to the map
+    lock: sync.Mutex{},
+  }
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+  i.lock.Lock()
+  defer i.lock.Unlock()
   i.store[name]++
 }
 
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+  i.lock.Lock()
+  defer i.lock.Unlock()
   return i.store[name]
 }
